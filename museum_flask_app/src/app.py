599cbbdee92379f6b5978dc9d0d029437918d974
Maxwell <mgdielman@gmail.com>
2023-10-09 10:35:47 -0400

    Added additional forms

--- museum_flask_app/src/app.py
+++ museum_flask_app/src/app.py
@@ -7,13 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
+    <select name="cars">
+        <option value="Harvard">Harvard Museum of Art</option>
+        <option value="NationalGallery">National Gallery of Art</option>
+        <option value="Smithsonian">Smithsonian</option>
+    </select>
     <form action="/echo_user_input" method="POST">
         <input name="user_input">
         <input type="submit" value="Submit!">
     </form>
-    <form action="/echo_user_input" method="POST">
-        <input name="user_input">
-        <input type="submit" value="Submit!">
+    <form action="/authentication" method="POST">
+        <input type="submit" value="Login/Register" name="auth_action"/>
     </form>
      '''
 
@@ -21,3 +25,13 @@ def main():
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
+
+@app.route("/authentication", methods=["POST"])
+def echo_input():
+    return '''
+    Sorry, Authentication is not yet implemented.
+    <form action="/" method="POST">
+        <input type="submit" value="Back to Home" name="home_action"/>
+    </form>
+     '''
+
