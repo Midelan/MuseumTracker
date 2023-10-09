e1ad3c6bd9fa4e58190f4d972593f3fb4df28d3b
Maxwell <mgdielman@gmail.com>
2023-10-09 11:10:36 -0400

    Added extra text for usability, and back menu

--- museum_flask_app/src/app.py
+++ museum_flask_app/src/app.py
@@ -8,11 +8,21 @@ app = Flask(__name__)
 def main():
     return '''
     <form action="/echo_user_input" method="POST">
+        Select Museum
+        <br>
         <select name="Museum">
             <option value="Harvard Museum of Art">Harvard Museum of Art</option>
             <option value="National Gallery of Art">National Gallery of Art</option>
             <option value="Smithsonian">Smithsonian</option>
         </select>
+        <br>
+        Select Mode
+        <br>
+        <select name="Mode">
+            <option value="New">Get New Records</option>
+            <option value="All">Get All Records</option>
+        </select>
+        <br>
         <input type="submit" value="Submit!">
     </form>
     <form action="/authentication" method="POST">
@@ -22,14 +32,28 @@ def main():
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("Museum", "")
-    return "Sorry, data for the " + input_text + " is not yet available"
+    museum_name = request.form.get("Museum", "")
+    mode = request.form.get("Mode", "")
+    if(mode == "All"):
+        return '''
+        Sorry, full data for the ''' + museum_name + ''' is not yet available
+        <form action="/">
+            <input type="submit" value="Back to Home" name="home_action"/>
+        </form>
+         '''
+    else:
+        return '''
+        Sorry, new data for the ''' + museum_name + ''' is not yet available
+        <form action="/">
+            <input type="submit" value="Back to Home" name="home_action"/>
+        </form>
+         '''
 
 @app.route("/authentication", methods=["POST"])
 def auth():
     return '''
     Sorry, Authentication is not yet implemented.
-    <form action="/" method="POST">
+    <form action="/">
         <input type="submit" value="Back to Home" name="home_action"/>
     </form>
      '''
