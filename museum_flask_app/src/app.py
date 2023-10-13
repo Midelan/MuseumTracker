bb5d70bf36cf9ca3a7fb5ac3146d59f35c993727
Maxwell <mgdielman@gmail.com>
2023-10-13 09:14:10 -0400

    Added datetime check

--- museum_flask_app/src/app.py
+++ museum_flask_app/src/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from datetime import datetime
 
 import psycopg2
 from flask import Flask, request, render_template
@@ -29,6 +30,7 @@ with app.app_context():
     }
     scheduler = BackgroundScheduler(executors=executors)
     print(scheduler.get_jobs())
+    print(datetime.now())
     job = scheduler.add_job(api_controller, trigger='cron', hour='9', minute='12', misfire_grace_time=60*60)
     scheduler.print_jobs()
     scheduler.start()
